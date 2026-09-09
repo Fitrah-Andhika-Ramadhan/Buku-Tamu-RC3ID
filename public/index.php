07d26877a2f1615bce0bc37520678a7410c1a493
Fitrah-Andhika-Ramadhan <fitrahramdhan31@gmail.com>
2026-09-09 20:01:41 +0700

    Add force cache and force migrate routes

--- public/index.php
+++ public/index.php
@@ -3,20 +3,33 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
-define('LARAVEL_START', microtime(true));
-
-if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/check-env') !== false) {
-    $envPath = __DIR__ . '/../.env';
-    if (file_exists($envPath)) {
-        die("File .env DITEMUKAN di: " . $envPath . "<br><br>Isinya:<br>" . nl2br(file_get_contents($envPath)));
-    } else {
-        die("File .env TIDAK DITEMUKAN di: " . $envPath . "<br><br>Tolong buat file .env di root (sejajar dengan folder app, bootstrap, public).");
+if (isset($_SERVER['REQUEST_URI'])) {
+    if (strpos($_SERVER['REQUEST_URI'], '/check-env') !== false) {
+        $envPath = __DIR__ . '/../.env';
+        if (file_exists($envPath)) {
+            die("File .env DITEMUKAN di: " . $envPath . "<br><br>Isinya:<br>" . nl2br(file_get_contents($envPath)));
+        } else {
+            die("File .env TIDAK DITEMUKAN.");
+        }
     }
-}
+    
+    if (strpos($_SERVER['REQUEST_URI'], '/force-cache') !== false) {
+        require __DIR__.'/../vendor/autoload.php';
+        $app = require_once __DIR__.'/../bootstrap/app.php';
+        $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+        $kernel->bootstrap();
+        \Illuminate\Support\Facades\Artisan::call('config:cache');
+        die("Config Cache berhasil dibuat! Silakan buka /force-migrate sekarang.");
+    }
 
-if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], '/force-migrate') !== false) {
-    // try to migrate outside of web middleware
-    // we will rely on artisan
+    if (strpos($_SERVER['REQUEST_URI'], '/force-migrate') !== false) {
+        require __DIR__.'/../vendor/autoload.php';
+        $app = require_once __DIR__.'/../bootstrap/app.php';
+        $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+        $kernel->bootstrap();
+        \Illuminate\Support\Facades\Artisan::call('migrate', ['--force' => true]);
+        die("Migrasi MySQL Berhasil! Output: " . \Illuminate\Support\Facades\Artisan::output());
+    }
 }
 
 // Determine if the application is in maintenance mode...
